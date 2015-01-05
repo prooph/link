09fae2518055e0a3656c8befb61705478ebc1623
Codeliner <kontakt@codeliner.ws>
2015-01-05 22:23:52 +0100

    Use constants for metadata in DoctrineTableGateway

--- module/SqlConnector/src/Service/DoctrineTableGateway.php
+++ module/SqlConnector/src/Service/DoctrineTableGateway.php
@@ -33,6 +33,86 @@ use Zend\XmlRpc\Value\AbstractCollection;
 final class DoctrineTableGateway implements WorkflowMessageHandler
 {
     /**
+     * Key in collect-data single result metadata. If set to TRUE the TableGateway uses the identifier name of the ginger type to find a result
+     */
+    const META_IDENTIFIER = 'identifier';
+
+    /**
+     * Key in collect-data metadata. Key-Value-Pairs of filters. Multiple filters are combined to a And-Filer.
+     * If key is a table column and value a scalar value a key = value (aka column = value) filter is applied.
+     * If value is an array it should contain a operand and value definition and optional a column definition if key is not the column name.
+     * The latter is useful when specifying a between filter:
+     *
+     * filter = [
+     *   'age_min' => [
+     *     'column' => 'age',
+     *     'operand' => '>=',
+     *     'value' => 21
+     *   ],
+     *   'age_max' => [
+     *     'column' => 'age',
+     *     'operand' => '<=',
+     *     'value' => 120
+     *   ]
+     * [
+     */
+    const META_FILTER = "filter";
+
+    /**
+     * Filter keys. See example above
+     */
+    const FILTER_COLUMN = "column";
+    const FILTER_OPERAND = "operand";
+    const FILTER_VALUE  = "value";
+
+    /**
+     * Available operands for a filter.
+     */
+    const OPERAND_EQ      = "=";
+    const OPERAND_LT      = "<";
+    const OPERAND_LTE     = "<=";
+    const OPERAND_GTE     = ">=";
+    const OPERAND_GT      = ">";
+    const OPERAND_LIKE    = "like";
+    const OPERAND_LIKE_CI = "ilike";
+
+    /**
+     * If filters are not enough to fetch the right data you can also provide a link to a file that provides a callable which modifies the query.
+     * The callable should take two arguments the Doctrine\DBAL\Query\QueryBuilder and the metadata array.
+     * It should directly operate on the query builder.
+     *
+     * Note: The table of the requested ginger type is aliased as "main"
+     * Note: Don't set limit, offset or order_by. Use metadata for them, because the TableGateway performs also a count query without offset, limit and order_by
+     *       but with all filters applied.
+     *
+     * example query builder script:
+     * <code>
+     * //Return a callable from the script
+     * return function(\Doctrine\DBAL\Query\QueryBuilder $query, array $metadata) {
+     *   $query->orWhere($query->exp()->eq('vip', 1));
+     * };
+     * </code>
+     */
+    const META_QUERY_BUILDER_SCRIPT = "query_builder_script";
+
+    /**
+     * Set the query offset.
+     */
+    const META_OFFSET = "offset";
+
+    /**
+     * Set the query limit
+     */
+    const META_LIMIT = "limit";
+
+    /**
+     * Set order_by like you would do in a SQL query.
+     *
+     * example: "age DESC,name ASC"
+     */
+    const META_ORDER_BY = "order_by";
+
+    /**
      * @var CommandBus
      */
     private $commandBus;
@@ -118,7 +198,7 @@ final class DoctrineTableGateway implements WorkflowMessageHandler
 
         $query = $this->buildQueryFromMetadata($itemType, $metadata);
 
-        if (isset($metadata['identifier'])) {
+        if (isset($metadata[self::META_IDENTIFIER])) {
             if (! $itemType::prototype()->typeDescription()->hasIdentifier()) throw new \InvalidArgumentException(sprintf("Item type %s has no identifier", $itemType));
 
             $identifierName = $itemType::prototype()->typeDescription()->identifierName();
@@ -196,45 +276,45 @@ final class DoctrineTableGateway implements WorkflowMessageHandler
 
         $query->from($this->table, 'main');
 
-        if (isset($metadata['filter']) && is_array($metadata['filter'])) {
+        if (isset($metadata[self::META_FILTER]) && is_array($metadata[self::META_FILTER])) {
             $filterCount = 0;
 
-            foreach ($metadata['filter'] as $column => $value) {
+            foreach ($metadata[self::META_FILTER] as $column => $value) {
                 $filterCount++;
 
                 if (is_array($value)) {
-                    if (! array_key_exists('operand', $value)) throw new \InvalidArgumentException(sprintf('Missing operand in filter array for column %s', $column));
-                    if (! array_key_exists('value', $value)) throw new \InvalidArgumentException(sprintf('Missing value in filter array for column %s', $column));
+                    if (! array_key_exists(self::FILTER_OPERAND, $value)) throw new \InvalidArgumentException(sprintf('Missing operand in filter array for column %s', $column));
+                    if (! array_key_exists(self::FILTER_VALUE, $value)) throw new \InvalidArgumentException(sprintf('Missing value in filter array for column %s', $column));
 
-                    if (isset($value['column'])) {
-                        $column = $value['column'];
+                    if (isset($value[self::FILTER_COLUMN])) {
+                        $column = $value[self::FILTER_COLUMN];
                     }
 
                     $column = $itemType::toDbColumnName($column);
 
-                    $this->addFilter($query, $column, $value['operand'], $value['value'], $filterCount);
+                    $this->addFilter($query, $column, $value[self::FILTER_OPERAND], $value[self::FILTER_VALUE], $filterCount);
                 } else {
                     $this->addFilter($query, $column, '=', $value, $filterCount);
                 }
             }
         }
 
-        if (isset($metadata['query_builder_script']) && file_exists($metadata['query_builder_script'])) {
-            $queryBuilderFunc = include($metadata['query_builder_script']);
+        if (isset($metadata[self::META_QUERY_BUILDER_SCRIPT]) && file_exists($metadata[self::META_QUERY_BUILDER_SCRIPT])) {
+            $queryBuilderFunc = include($metadata[self::META_QUERY_BUILDER_SCRIPT]);
             if (is_callable($queryBuilderFunc)) $queryBuilderFunc($query, $metadata);
         }
 
         if (! $countMode) {
-            if (isset($metadata['offset'])) {
-                $query->setFirstResult($metadata['offset']);
+            if (isset($metadata[self::META_OFFSET])) {
+                $query->setFirstResult($metadata[self::META_OFFSET]);
             }
 
-            if (isset($metadata['limit'])) {
-                $query->setMaxResults($metadata['limit']);
+            if (isset($metadata[self::META_LIMIT])) {
+                $query->setMaxResults($metadata[self::META_LIMIT]);
             }
 
-            if (isset($metadata['order_by'])) {
-                $orderByArr = explode(",", $metadata['order_by']);
+            if (isset($metadata[self::META_ORDER_BY])) {
+                $orderByArr = explode(",", $metadata[self::META_ORDER_BY]);
 
                 foreach ($orderByArr as $orderBy) {
                     $orderBy = trim($orderBy);
@@ -263,25 +343,25 @@ final class DoctrineTableGateway implements WorkflowMessageHandler
     private function addFilter(QueryBuilder $query, $column, $operand, $value, $filterCount)
     {
         switch ($operand) {
-            case '=':
+            case self::OPERAND_EQ:
                 $query->andWhere($query->expr()->eq($column, ':__filter' . $filterCount));
                 break;
-            case '<':
+            case self::OPERAND_LT:
                 $query->andWhere($query->expr()->lt($column, ':__filter' . $filterCount));
                 break;
-            case '<=':
+            case self::OPERAND_LTE:
                 $query->andWhere($query->expr()->lte($column, ':__filter' . $filterCount));
                 break;
-            case '>':
+            case self::OPERAND_GT:
                 $query->andWhere($query->expr()->gt($column, ':__filter' . $filterCount));
                 break;
-            case '>=':
+            case self::OPERAND_GTE:
                 $query->andWhere($query->expr()->gte($column, ':__filter' . $filterCount));
                 break;
-            case 'like':
+            case self::OPERAND_LIKE:
                 $query->andWhere($query->expr()->like($column, ':__filter' . $filterCount));
                 break;
-            case 'ilike':
+            case self::OPERAND_LIKE_CI:
                 $query->andWhere($query->expr()->like('LOWER(' . $column . ')', ':__filter' . $filterCount));
                 $value = strtolower($value);
                 break;
